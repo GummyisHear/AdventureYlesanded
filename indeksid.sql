3ded0c1ca4fe4948eaab127bd592eb818432616b
artjompoldsaar <artjompoldsaar@gmail.com>
2025-10-06 10:36:35 +0300

    indeksid ulesanned tehtud

--- indeksid.sql
+++ indeksid.sql
@@ -9,4 +9,7 @@ where BaseRate > 20 and BaseRate < 50;
 --Nüüd loome indeksi, mis aitab päringut: Loome indeksi Salary veerule.
 create index IX_DimEmployee_BaseRate
 on DimEmployee(BASERATE ASC)
-
+-- kuvab kõik indeksid tabelis
+Execute sp_helptext DimEmployee
+--kustutame indeks
+drop index DimEmployee.IX_DimEmployee_BaseRate
