30cac79283fd2ce800e7105d542c1007280b20ea
artjompoldsaar <artjompoldsaar@gmail.com>
2025-10-09 15:05:48 +0300

    except ja not in operaatori erinevus

--- except.sql
+++ except.sql
@@ -66,3 +66,65 @@ select Id, Name, Gender, Salary
 from tblEmployees
 where salary >= 60000
 order by Name;
+
+
+----------------------------------------------------------------------
+--88. Erinevus Except ja not in operaatoril
+
+--Järgnev päring tagastab read vasakust päringust, mis ei ole paremas tabelis
+select Id, Name, Gender from TableA
+except
+select Id, Name, Gender from TableB
+
+--Sama tulemuse võib saavutada NOT IN operaatoriga:
+select Id, Name, Gender from TableA
+where Id not in (select Id from TableB)
+
+--Mis erinevus on neil kahel operaatoril.
+--Except filtreerib korduvaid andmeid ja tagastab ainult erinevaid ridu vasakust päringust, mis ei ole parema rea päringu tulemuses. 
+--NOT IN ei filtreeri duplikaate.
+
+--lisame duplikaat tabelile A
+insert into TableA values (1, 'Mark', 'Male');
+
+--nüüd käivitame sama päring
+select Id, Name, Gender from TableA
+except
+select Id, Name, Gender from TableB
+
+--nüüd käivita NOT IN opetaatoriga, näitab duplikaati
+select Id, Name, Gender from TableA
+where Id not in (select Id from TableB)
+
+--EXCEPT operaator  ootab sama arvu veerge mõlemas päringus ja NOT IN ei oota seda.
+--NOT IN võrdleb üksikut veergu välisest päringust koos üksiku veeruga alampäringust.
+
+--Järgnevas päringus on meelega veergude arv erinev
+select Id, Name, Gender from TableA
+except
+select Id, Name from TableB
+--veateate
+
+--Järgnevas päringus alampäring tagastab mitu veergu:
+select Id, Name, Gender from TableA
+where Id not in (select Id, Name from TableB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
